41f7378054627a054a8204bb41417531c77a617c
Vanama Karthik <151547677+vanamakarthik351@users.noreply.github.com>
2026-07-28 15:58:18 +0530

    Time: 0 ms (100%), Space: 42.1 MB (52.64%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int fib(int n) {
+        if(n==1){
+            return 1;
+        }
+        if(n==0){
+            return 0;
+        }
+        int sum=0;
+        int first=0;
+        int second=1;
+        for(int i=2;i<=n;i++){
+            sum=first+second;
+            first=second;
+            second=sum;
+        }
+        return sum;
+    }
+}
